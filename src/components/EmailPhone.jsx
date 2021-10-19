9413b5916cb8942b9a6602cf15fd245af66fb151
David Verduzco <67717053+tangentdigitalagency@users.noreply.github.com>
2021-10-19 11:00:55 -0700

    change verbiage

--- src/components/EmailPhone.jsx
+++ src/components/EmailPhone.jsx
@@ -220,7 +220,7 @@ class EmailPhone extends Component {
                                     <div>
                                         <div className="relative flex justify-center text-sm leading-5 con">
                                             <span className="px-2 text-black-500 text-3xl bold header">
-                                                You're Almost There, {firstName}!
+                                                {firstName}, Last Step!
                                             </span>
                                         </div>
                                     </div>
